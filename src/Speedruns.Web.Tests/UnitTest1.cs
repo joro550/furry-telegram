821815f8478c00017eafe2528b6c8bcb722de7e0
Mark Davies <markdavies550@gmail.com>
2019-12-13 20:39:57 +0000

    Adding test assemblies

---
+++ src/Speedruns.Web.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Speedruns.Web.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
--- src/Speedruns.Web/Startup.cs
+++ src/Speedruns.Web/Startup.cs
@@ -1,59 +1,54 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Speedruns.Web.Areas.Identity;
-using Speedruns.Web.Data;
+using Speedruns.Web.Users;
 
 namespace Speedruns.Web
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            _configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
+                options.UseSqlServer(_configuration.GetConnectionString("DefaultConnection")));
+
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
-            services.AddSingleton<WeatherForecastService>();
+            services
+                .AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>
+                >();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseDatabaseErrorPage();
+
+                CatalogContextSeed.SeedAsync(app)
+                    .Wait();
             }
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
@@ -71,6 +66,31 @@ namespace Speedruns.Web
                 endpoints.MapBlazorHub();
                 endpoints.MapFallbackToPage("/_Host");
             });
+        }
+    }
+
+    public class CatalogContextSeed
+    {
+        public static async Task SeedAsync(IApplicationBuilder applicationBuilder)
+        {
+            // var context = (CatalogContext)applicationBuilder
+            //     .ApplicationServices.GetService(typeof(CatalogContext));
+            // using (context)
+            // {
+            //     context.Database.Migrate();
+            //     if (!context.CatalogBrands.Any())
+            //     {
+            //         context.CatalogBrands.AddRange(
+            //             GetPreconfiguredCatalogBrands());
+            //         await context.SaveChangesAsync();
+            //     }
+            //     if (!context.CatalogTypes.Any())
+            //     {
+            //         context.CatalogTypes.AddRange(
+            //             GetPreconfiguredCatalogTypes());
+            //         await context.SaveChangesAsync();
+            //     }
+            // }
         }
     }
 }
