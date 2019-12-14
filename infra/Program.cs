a239f9353bbb8d425386b6046a00026a093de37b
Mark Davies <markdavies550@gmail.com>
2019-12-14 17:55:42 +0000

    Adding a function to update runner online status

---
+++ infra/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Pulumi;
+using Pulumi.Azure.Core;
+using Pulumi.Azure.Storage;
+
+namespace Infra
+{
+    internal static class Program
+    {
+        private static Task<int> Main()
+        {
+            return Deployment.RunAsync(() => {
+
+                // Create an Azure Resource Group
+                var resourceGroup = new ResourceGroup("resourceGroup");
+
+                // Create an Azure Storage Account
+                var storageAccount = new Account("storage", new AccountArgs
+                {
+                    ResourceGroupName = resourceGroup.Name,
+                    AccountReplicationType = "LRS",
+                    AccountTier = "Standard",
+                });
+
+                // Export the connection string for the storage account
+                return new Dictionary<string, object>
+                {
+                    { "connectionString", storageAccount.PrimaryConnectionString },
+                };
+            });
+        }
+    }
+}
