738c119211502e7c52f72619de030d192bed87a1
Mark Davies <mdavies2@webroot.com>
2020-01-09 15:31:28 +0000

    Working on stream presentation functionality

--- src/Speedruns.Web/Data/Stream.cs
+++ src/Speedruns.Web/Data/Stream.cs
@@ -1,4 +1,7 @@
-﻿namespace Speedruns.Web.Data
+﻿using System.Threading.Tasks;
+using Microsoft.JSInterop;
+
+namespace Speedruns.Web.Data
 {
     public class Stream
     {
@@ -7,5 +10,8 @@
         public bool IsOnline { get; set; }
         public string Platform { set; get; }
         public string Description { get; set; }
+
+        public async Task PresentStream(IJSRuntime runtime, string elementId) 
+            => await runtime.InvokeVoidAsync("embedTwitch", elementId, Username);
     }
 }
